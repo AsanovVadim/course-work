a0e1a0828a3125fe9b4eff777957512a4cb1da47
AsanovVadim <vadimka2-9@mail.ru>
2021-10-27 02:24:53 +0300

    add extension functions for vector

---
+++ VectorLib/VectorLib.h
@@ -0,0 +1,47 @@
+#ifndef COURSEWORK_VECTORLIB_H
+#define COURSEWORK_VECTORLIB_H
+
+#include <vector>
+#include <numeric>
+#include <cmath>
+
+template <typename T>
+T Dot(const std::vector<T>& v1, const std::vector<T>& v2) {
+    double res = 0;
+    for (size_t i = 0; i < v1.size(); ++i) {
+        res += v1[i] * v2[i];
+    }
+    return res;
+}
+
+template <typename T>
+std::vector<T> operator- (const std::vector<T>& v1, const std::vector<T>& v2) {
+    std::vector<T> res(v1.size());
+    for (size_t i = 0; i < v1.size(); ++i) {
+        res[i] = v1[i] - v2[i];
+    }
+    return res;
+}
+
+template <typename NumberType>
+NumberType Norm(const std::vector<NumberType>& v, int order) {
+    NumberType init = 0;
+    if (order == 1) {
+        return std::accumulate(v.begin(), v.end(), init, [](auto x, auto y) { return x + std::abs(y); });
+    }
+    if (order == 2) {
+        return std::sqrt(std::accumulate(v.begin(), v.end(), init, [](auto x, auto y) { return x + y*y; }));
+    }
+    return 0;
+}
+
+template <typename NumberType>
+std::vector<NumberType> operator* (NumberType num, const std::vector<NumberType>& v) {
+    std::vector<NumberType> res(v.size());
+    for (size_t i = 0; i < v.size(); ++i) {
+        res[i] = v[i] * num;
+    }
+    return res;
+}
+
+#endif //COURSEWORK_VECTORLIB_H
